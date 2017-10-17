992bf238576ba792bd52c5ecaa060b7305abba9a
Andrey Stepnov <myjamm@gmail.com>
2017-10-17 18:45:04 +1100

    Add types and read_vs30_file function

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gmm.jl
 The ground motion evaluation module
+
+## How to get VS30 grid
+
+1. Download GMT grd file for Global VS30 data on [USGS site](https://earthquake.usgs.gov/data/vs30/)
+2. Unzip `unzip global_vs30_grd.zip`. It take around 2,7G disk space for one file: `2,7G  8 сен  2016 global_vs30.grd`.
+3. Using `GMT2XYZ` [man page](https://www.soest.hawaii.edu/gmt/gmt/html/man/grd2xyz.html) from [GMT](https://www.soest.hawaii.edu/gmt/) to convert grd data to XYZ test file:
+```
+# example:
+grd2xyz global_vs30.grd -R145.0/146.0/50.0/51.0 > test_sea.txt
+# number of rows:
+cat test_sea.txt |wc -l
+   14641
+```
+
+
--- src/GMM.jl
+++ src/GMM.jl
@@ -3,30 +3,33 @@ Module GMM
 	# The distance() function is used, see https://github.com/JuliaGeo/Geodesy.jl for further read
 	using Geodesy
 
-	# define types
+	#****************
+	#*    TYPES     *
+	#****************
 	## VS30 data point
-	mutable struct vs30_point
-		lat::Float64
+	mutable struct VS30_point
 		lon::Float64
+		lat::Float64
 		VS30::Float64
 	end
 	## Output PGA data point after GMPE calculations
-	mutable struct gmm_point_pga
-		lat::Float64
+	mutable struct Gmm_point_pga
 		lon::Float64
+		lat::Float64
 		r_rup::Float64
 		g::Float64 #Acceleration of gravity in percent rounded to ggg.gg
 	end
 	## earthquake location data
-	mutable struct earthquake
-		lat::Float64
+	mutable struct Earthquake
 		lon::Float64
+		lat::Float64
 		depth::Float64
 		local_mag::Float64
 		moment_mag::Float64
 	end
+	Earthquake(x,y,z,k) = Earthquake(x,y,z,k,0) # Mw usually not ready right after earthquake 
 	## AS2008 GMPE paramaters
-	mutable struct as2008_params
+	mutable struct AS2008_params
 		a1::Float64
   	a2::Float64
   	a3::Float64
@@ -46,9 +49,9 @@ Module GMM
 		t6::Float64
 	end
 	## AS2008 GMPE data point
-	mutable struct as2008_point
-		lat::Float64
+	mutable struct AS2008_point
 		lon::Float64
+		lat::Float64
 		vs30::Float64
 		r_rup::Float64
 		f1::Float64
@@ -58,6 +61,26 @@ Module GMM
 		g::Float64 # Acceleration of gravity in fractions
 	end
 
+	#****************
+	#*  FUNCTIONS   *
+	#****************
+	## read VS30 file
+	function read_vs30_file(filename::String)
+		@time A = readdlm(filename)
+		vs30_row_num = length(A[:,1])
+		@time B = Array{VS30_point}(0)
+		@time for i=1:vs30_row_num
+			B = push!(B,VS30_point(A[i,1],A[i,2],A[i,3]))
+		end
+		return B
+	end
+
+
+
+
+
+
 
 
 end #module
+
