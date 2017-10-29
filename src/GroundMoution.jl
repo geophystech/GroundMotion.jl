c6e8716593dd3089c5bed7dee18ff14714b694be
Andrey Stepnov <myjamm@gmail.com>
2017-10-30 01:21:15 +1100

    fix name

---
+++ src/GroundMoution.jl
@@ -0,0 +1,27 @@
+module GroundMoution
+  # This source contains a module settings. Please find types and functions in nested source files (see include).
+
+  # The distance() function is used, see https://github.com/JuliaGeo/Geodesy.jl for further read
+  using 
+    Geodesy
+
+  export
+    # types
+    Point_vs30,
+    Point_gmpe_out,
+    Earthquake,
+    Params_as2008,
+    # functions
+    read_vs30_file,
+    convert_to_float_array,
+    gmpe_as2008
+
+  # Types
+  include("types.jl")
+  # AS2008 PGA modeling
+  include("as2008.jl")
+  # read/write functions
+  include("auxiliary.jl")
+
+end
+
